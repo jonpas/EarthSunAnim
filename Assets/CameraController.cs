e37f28b83aa5eada2ed806f5cd0ada42dddfc63c
jonpas <jonpas33@gmail.com>
2018-03-17 03:45:42 +0100

    Shorten Camera Controller code

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,20 +3,15 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     private static float movementSpeed = 1.0f;
-    private static float mouseX = 0.0f;
-    private static float mouseY = 0.0f;
 
     // Update is called once per frame
     void Update() {
         movementSpeed = Mathf.Max(movementSpeed += Input.GetAxis("Mouse ScrollWheel"), 0.0f);
         transform.position += (transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical") + transform.up * Input.GetAxis("Depth")) * movementSpeed;
         if (Input.GetMouseButton(1)) {
-            mouseX = -Input.GetAxis("Mouse Y");
-            mouseY = Input.GetAxis("Mouse X");
+            transform.eulerAngles += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), Input.GetAxis("Rotation"));
         } else {
-            mouseX = 0.0f;
-            mouseY = 0.0f;
+            transform.eulerAngles += new Vector3(0.0f, 0.0f, Input.GetAxis("Rotation"));
         }
-        transform.eulerAngles += new Vector3(mouseX, mouseY, Input.GetAxis("Rotation"));
     }
 }
